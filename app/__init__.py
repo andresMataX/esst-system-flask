f75958f84c2b109a13be88795acc5e43835bc267
Andrés Mata <andres_mata21@outlook.com>
2022-05-05 16:56:27 -0500

    Configuración de variables de entorno: init.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,15 @@ def create_app():
     # Instancia de Flask
     app = Flask(__name__)
 
+    # Configuración de variables de entorno
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+    )
+
     CORS(app)
 
     # Función de prueba
