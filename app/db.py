2cecc643c17632fe7d084c7d0b466f02d99d99a8
Andrés Mata <andres_mata21@outlook.com>
2022-05-05 17:03:41 -0500

    Importación de dependencias: db.py

---
+++ app/db.py
@@ -0,0 +1,8 @@
+import mysql.connector
+# click es para poder usar un comando en consola
+import click
+# Variable g que es global
+from flask import current_app, g
+from flask.cli import with_appcontext
+# Instrucciones para crear la base de datos
+# TODO: crear achivo schema para las instrucciones de creación de base de datos
