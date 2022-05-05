6df96ff8e414e38577cacc318e772b5f4fde0008
Andrés Mata <andres_mata21@outlook.com>
2022-05-05 18:32:07 -0500

    Creación de blueprint de autenticación: auth.py

--- app/auth.py
+++ app/auth.py
@@ -6,3 +6,6 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 # Interactuamos con la base de datos
 from app.db import get_db
+
+# Creamos el blueprint de autenticación
+bp = Blueprint('auth', __name__, url_prefix='/auth')
