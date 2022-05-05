cf88dcdc42b24ce9a31297b6edb93231d741e6ba
Andrés Mata <andres_mata21@outlook.com>
2022-05-05 17:12:25 -0500

    Instruciones para crear tablas: schema.py

--- app/schema.py
+++ app/schema.py
@@ -7,4 +7,49 @@ instructions = [
     'DROP TABLE IF EXISTS Coste;',
     'DROP TABLE IF EXISTS Producto;',
     'SET FOREIGN_KEY_CHECKS=1;',
+    # Creación de tablas
+    """
+    CREATE TABLE Usuario(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        user VARCHAR(50) UNIQUE NOT NULL,
+        pass VARCHAR(255) NOT NULL
+    );
+    """,
+    """
+    CREATE TABLE Corte(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        cut_name VARCHAR(15) NOT NULL,
+        cut_price INT NOT NULL
+    );
+    """,
+    """
+    CREATE TABLE Venta(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name_cli VARCHAR(15) NOT NULL,
+        l_name_cli VARCHAR(15) NOT NULL,
+        id_cut_type INT NOT NULL,
+        id_user INT NOT NULL,
+        date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(id_cut_type) REFERENCES Corte(id),
+        FOREIGN KEY(id_user) REFERENCES Usuario(id)
+    );
+    """,
+    """
+    CREATE TABLE Producto(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        prod_name VARCHAR(15) NOT NULL,
+        prod_price INT NOT NULL
+    );
+    """,
+    """
+    CREATE TABLE Coste(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name_cost VARCHAR(15) NOT NULL,
+        id_pro_type INT NOT NULL,
+        id_user INT NOT NULL,
+        date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(id_pro_type) REFERENCES Producto(id),
+        FOREIGN KEY(id_user) REFERENCES Usuario(id)
+    );
+    """
 ]
